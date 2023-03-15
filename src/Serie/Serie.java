91c9ef4617f843feed5c8c84a55e6672bc6b67a3
yamir <yamir@mandragora.mandragora>
2023-03-15 09:53:54 +0100

    Modificación de la clase Serie

--- src/Serie/Serie.java
+++ src/Serie/Serie.java
@@ -1,21 +1,20 @@
 package Serie;
 
 public class Serie {
-	
+
 	// constantes que indican el valor por defecto
 	final private String TITULO_POR_DEFECTO = "";
 	final private int TEMPORADAS_POR_DEFECTO = 3;
 	final private boolean ENTREDAGO_POR_DEFECTO = false;
 	final private String GENERO_POR_DEFECTO = "";
 	final private String CREADOR_POR_DEFECTO = "";
-	
-	
+
 	private String titulo;
 	private int numero_temporadas;
 	private boolean entregado;
 	private String genero;
 	private String creador;
-	
+
 	public Serie() {
 		this.titulo = this.TITULO_POR_DEFECTO;
 		this.numero_temporadas = this.TEMPORADAS_POR_DEFECTO;
@@ -23,7 +22,7 @@ public class Serie {
 		this.creador = this.CREADOR_POR_DEFECTO;
 		this.entregado = this.ENTREDAGO_POR_DEFECTO;
 	}
-	
+
 	public Serie(String titulo, String creador) {
 		this.titulo = titulo;
 		this.numero_temporadas = this.TEMPORADAS_POR_DEFECTO;
@@ -31,7 +30,7 @@ public class Serie {
 		this.creador = creador;
 		this.entregado = this.ENTREDAGO_POR_DEFECTO;
 	}
-	
+
 	public Serie(String titulo, int numero_temporadas, String genero, String creador) {
 		this.titulo = titulo;
 		this.numero_temporadas = numero_temporadas;
@@ -40,16 +39,19 @@ public class Serie {
 		this.entregado = this.ENTREDAGO_POR_DEFECTO;
 	}
 
+	@Override
+	public String toString() {
+		return "Título: " + this.titulo + "\n" + "Número de temporadas: " + this.numero_temporadas + "\n"
+				+ "Entregado: " + this.entregado + "\n" + "Género: " + this.genero + "\n" + "Creador: " + this.creador
+				+ "\n";
+	}
+
 	public String getTitulo() {
 		return titulo;
 	}
 
 	public int getNumero_temporadas() {
 		return numero_temporadas;
-	}
-
-	public boolean isEntregado() {
-		return entregado;
 	}
 
 	public String getGenero() {
@@ -68,10 +70,6 @@ public class Serie {
 		this.numero_temporadas = numero_temporadas;
 	}
 
-	public void setEntregado(boolean entregado) {
-		this.entregado = entregado;
-	}
-
 	public void setGenero(String genero) {
 		this.genero = genero;
 	}
@@ -79,7 +77,5 @@ public class Serie {
 	public void setCreador(String creador) {
 		this.creador = creador;
 	}
-	
-	
-	
+
 }
